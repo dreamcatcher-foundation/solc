3f932eb9f93953efc3a849793af701ce26fcaa16
Marco Bizzaro <51394894+snqre@users.noreply.github.com>
2024-11-01 23:54:04 +0100

    implement selector

--- src/evm/_.ts
+++ src/evm/_.ts
@@ -1,2 +1,17 @@
+export type Account = {
+    address(): Promise<string>;
+    nonce(): Promise<number>;
+    nextNonce(): Promise<number>;
+    
+};
+
+export type Evm = {
+
+};
+
+export function Evm(): Evm {
+
+}
+
 export * as Dispatch from "./dispatch/_";
 export * as TypeSystem from "./type-system/_";
--- src/evm/dispatch/selector.ts
+++ src/evm/dispatch/selector.ts
@@ -1,1 +1,14 @@
+import {DataType} from "@this/evm/type-system";
+
 export type Selector = `${string}(${string})`;
+
+export function Selector(name: string, ... args: DataType[]): Selector {
+    let args_: string = "";
+    let i: bigint = 0n;
+    while (Number(i) < args.length) {
+        if (i !== 0n) args_ = ", ";
+        args_ += args[Number(i)];
+        i += 1n;
+    }
+    return `${name}(${args_})`;
+}
